0d0fa3d8fb4e0468325c4fa16654051b3afddb56
Jeferson Coutinho <jefissuu@gmail.com>
2022-12-03 10:42:15 -0300

    add collapsing toolbar in home screen

--- app/src/main/java/com/jefisu/anlist/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/jefisu/anlist/presentation/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.jefisu.anlist.presentation.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -32,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -55,6 +59,9 @@ import com.jefisu.anlist.ui.theme.Platinum
 import com.jefisu.anlist.ui.theme.defaultTextStyle
 import com.jefisu.anlist.ui.theme.mirror
 import java.time.LocalDate
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 
 @Composable
 fun HomeScreen(
@@ -69,164 +76,187 @@ fun HomeScreen(
         IconSeasonSettings.Schedule
     )
     var text by remember { mutableStateOf("") }
+    val collapsingState = rememberCollapsingToolbarScaffoldState()
+    val alphaAnim by animateFloatAsState(
+        targetValue = collapsingState.toolbarState.progress
+    )
 
-    Box(
-        modifier = Modifier.verticalScroll(rememberScrollState())
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .clip(RoundedCornerShape(bottomStart = 24.dp))
-                .background(DarkSlateBlue)
-        ) {
-            Image(
-                painter = painterResource(R.drawable.ic_object),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(Liberty),
+    CollapsingToolbarScaffold(
+        modifier = Modifier.fillMaxSize(),
+        state = collapsingState,
+        scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+        toolbar = {
+            Box(
                 modifier = Modifier
-                    .width(291.04.dp)
-                    .height(95.dp)
-                    .align(Alignment.TopEnd)
-                    .graphicsLayer {
-                        translationY = -4.dp.toPx()
-                        translationX = 20.dp.toPx()
-                    }
-            )
-            Image(
-                painter = painterResource(R.drawable.ic_custom_circle),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(149.dp)
-                    .align(Alignment.BottomEnd)
-                    .graphicsLayer {
-                        translationY = 53.dp.toPx()
-                        translationX = 17.dp.toPx()
-                    }
-            )
-        }
-        Column {
-            Column(
-                modifier = Modifier.padding(start = 24.dp)
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(bottomStart = 24.dp))
+                    .background(DarkSlateBlue)
+                    .pin()
             ) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
+                Image(
+                    painter = painterResource(R.drawable.ic_object),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(Liberty),
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 12.dp, end = 12.dp)
-                ) {
-                    Text(
-                        text = stringResource(R.string.app_name),
-                        style = defaultTextStyle,
-                        fontSize = 16.sp,
-                        fontStyle = FontStyle.Italic
+                        .width(291.04.dp)
+                        .height(95.dp)
+                        .align(Alignment.TopEnd)
+                        .alpha(alphaAnim)
+                        .graphicsLayer {
+                            translationY = -4.dp.toPx()
+                            translationX = 20.dp.toPx()
+                        }
+                )
+                Image(
+                    painter = painterResource(R.drawable.ic_custom_circle),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(149.dp)
+                        .align(Alignment.BottomEnd)
+                        .graphicsLayer {
+                            translationY = 53.dp.toPx()
+                            translationX = 17.dp.toPx()
+                        }
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(top = 48.dp)
+                    .road(Alignment.TopStart, Alignment.TopStart)
+            ) {
+                Column(modifier = Modifier.padding(start = 24.dp)) {
+                    TextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        textStyle = defaultTextStyle.copy(
+                            fontSize = 14.sp,
+                            color = Color.Black
+                        ),
+                        placeholder = {
+                            Text(
+                                text = stringResource(R.string.search_anime_hint),
+                                style = defaultTextStyle,
+                                fontSize = 14.sp,
+                                color = Gray
+                            )
+                        },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = null,
+                                tint = Gray,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Platinum,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ),
+                        modifier = Modifier
+                            .width(294.75.dp)
+                            .height(53.dp)
+                            .clip(RoundedCornerShape(16.dp))
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(R.string.top_airing, LocalDate.now().year),
+                        style = defaultTextStyle,
+                        fontSize = 14.sp
+                    )
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+                LazyRow {
+                    items(topAirings) {
+                        CustomCard(
+                            anime = it,
+                            modifier = Modifier.padding(
+                                start = if (it == topAirings.first()) 24.dp else 0.dp,
+                                end = 12.dp
+                            )
+                        )
+                    }
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, end = 12.dp)
+                    .padding(vertical = 12.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = defaultTextStyle,
+                    fontSize = 16.sp,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier.weight(1f)
+                )
+                AnimatedVisibility(visible = collapsingState.toolbarState.progress < 0.15f) {
                     Icon(
-                        imageVector = Icons.Default.Sort,
+                        imageVector = Icons.Default.Search,
                         contentDescription = null,
                         tint = Color.White,
                         modifier = Modifier.mirror()
                     )
                 }
-                Spacer(modifier = Modifier.height(13.dp))
-                TextField(
-                    value = text,
-                    onValueChange = { text = it },
-                    textStyle = defaultTextStyle.copy(
-                        fontSize = 14.sp,
-                        color = Color.Black
-                    ),
-                    placeholder = {
-                        Text(
-                            text = stringResource(R.string.search_anime_hint),
-                            style = defaultTextStyle,
-                            fontSize = 14.sp,
-                            color = Gray
-                        )
-                    },
-                    leadingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = null,
-                            tint = Gray,
-                            modifier = Modifier.size(20.dp)
-                        )
-                    },
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Platinum,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
-                    ),
-                    modifier = Modifier
-                        .width(294.75.dp)
-                        .height(53.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = stringResource(R.string.top_airing, LocalDate.now().year),
-                    style = defaultTextStyle,
-                    fontSize = 14.sp
+                Spacer(modifier = Modifier.width(12.dp))
+                Icon(
+                    imageVector = Icons.Default.Sort,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.mirror()
                 )
             }
-            Spacer(modifier = Modifier.height(4.dp))
-            LazyRow {
-                items(topAirings) {
-                    CustomCard(
-                        anime = it,
-                        modifier = Modifier.padding(
-                            start = if (it == topAirings.first()) 24.dp else 0.dp
-                        )
-                    )
-                    Spacer(modifier = Modifier.width(12.dp))
-                }
-            }
-            Spacer(modifier = Modifier.height(24.dp))
-            Column(
-                modifier = Modifier.padding(horizontal = 24.dp)
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .padding(top = 24.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = stringResource(R.string.upcoming_year, LocalDate.now().year),
+                style = defaultTextStyle,
+                fontSize = 14.sp,
+                color = DarkSlateBlue
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = stringResource(R.string.upcoming_year, LocalDate.now().year),
-                    style = defaultTextStyle,
-                    fontSize = 14.sp,
-                    color = DarkSlateBlue
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    iconsSeason.forEach {
-                        CustomIcon(
-                            icon = it.icon,
-                            text = it.title,
-                        )
-                    }
+                iconsSeason.forEach {
+                    CustomIcon(
+                        icon = it.icon,
+                        text = it.title,
+                    )
                 }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = stringResource(R.string.recommend),
-                    style = defaultTextStyle,
-                    fontSize = 14.sp,
-                    color = DarkSlateBlue
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                FlowRow(
-                    mainAxisSpacing = 16.dp,
-                    crossAxisSpacing = 16.dp,
-                    modifier = Modifier.padding(bottom = 12.dp)
-                ) {
-                    recommends.forEach {
-                        Image(
-                            painter = painterResource(it),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(164.dp, 246.dp)
-                                .clip(RoundedCornerShape(12.dp))
-                        )
-                    }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(R.string.recommend),
+                style = defaultTextStyle,
+                fontSize = 14.sp,
+                color = DarkSlateBlue
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            FlowRow(
+                mainAxisSpacing = 16.dp,
+                crossAxisSpacing = 16.dp,
+                modifier = Modifier.padding(bottom = 12.dp)
+            ) {
+                recommends.forEach {
+                    Image(
+                        painter = painterResource(it),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(164.dp, 246.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                    )
                 }
             }
         }
