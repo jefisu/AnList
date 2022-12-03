7b5ccb5fda943cbd314b6081082402aa02937825
Jeferson Coutinho <jefissuu@gmail.com>
2022-12-03 01:25:49 -0300

    add collapsing toolbar in detail screen

--- app/src/main/java/com/jefisu/anlist/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/jefisu/anlist/presentation/detail/DetailScreen.kt
@@ -1,16 +1,25 @@
 package com.jefisu.anlist.presentation.detail
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,6 +34,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,6 +43,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
@@ -64,6 +75,9 @@ import com.jefisu.anlist.ui.theme.DarkSlateBlue
 import com.jefisu.anlist.ui.theme.GraniteGray
 import com.jefisu.anlist.ui.theme.PhilippineGray
 import com.jefisu.anlist.ui.theme.defaultTextStyle
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 
 @OptIn(ExperimentalPagerApi::class, ExperimentalAnimationApi::class)
 @Composable
@@ -77,71 +91,141 @@ fun DetailScreen(
     )
 
     val scrollState = rememberScrollState()
-    Box(
-        modifier = Modifier.verticalScroll(scrollState)
-    ) {
-        Image(
-            painter = painterResource(id = anime.imageBackground),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(
-                Color.Black.copy(0.3f),
-                blendMode = BlendMode.Luminosity
-            ),
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .height(228.dp)
-                .clip(RoundedCornerShape(0.dp, 0.dp, 24.dp, 24.dp))
-        )
-        IconButton(onClick = { /* TODO */ }) {
-            Icon(
-                imageVector = Icons.Default.ArrowBackIosNew,
-                contentDescription = null,
-                tint = Color.White
+    val collapsingState = rememberCollapsingToolbarScaffoldState()
+    val transition = updateTransition(collapsingState.toolbarState.progress, label = "")
+    val alphaAnim by transition.animateFloat(label = "") { progress ->
+        if (progress < 0.54f) 0f else 1f
+    }
+    val alphaBoxAnim by transition.animateFloat(label = "") { progress ->
+        if (progress == 0f) 1f else 0f
+    }
+
+    CollapsingToolbarScaffold(
+        modifier = Modifier.fillMaxSize(),
+        state = collapsingState,
+        scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+        toolbar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .pin()
+                    .background(DarkSlateBlue)
+                    .alpha(alphaBoxAnim)
             )
+            Image(
+                painter = painterResource(id = anime.imageBackground),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.Black.copy(0.3f), BlendMode.Luminosity),
+                contentScale = ContentScale.FillWidth,
+                alpha = if (collapsingState.toolbarState.progress < 0.1f) 0f else 1f,
+                modifier = Modifier
+                    .height(228.dp)
+                    .clip(RoundedCornerShape(0.dp, 0.dp, 24.dp, 24.dp))
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(top = 36.dp)
+                    .alpha(alphaAnim)
+                    .road(Alignment.TopCenter, Alignment.TopCenter)
+            ) {
+                CustomIcon(
+                    icon = R.drawable.ic_arrow_forward,
+                    text = stringResource(R.string.watch_trailer),
+                    size = 24.dp,
+                    shape = CircleShape,
+                    color = Color.Black.copy(0.5f),
+                    textStyle = defaultTextStyle.copy(
+                        fontSize = 14.sp,
+                        color = Color.White
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Image(
+                    painter = painterResource(anime.poster),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(202.dp)
+                        .clip(RoundedCornerShape(12.dp))
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = anime.name,
+                    style = defaultTextStyle,
+                    fontSize = 18.sp,
+                    color = DarkSlateBlue,
+                    maxLines = 2,
+                    textAlign = TextAlign.Center,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = anime.episodeDuration,
+                    style = defaultTextStyle,
+                    fontSize = 14.sp,
+                    color = PhilippineGray
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .pin()
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    IconButton(
+                        onClick = { /* TODO */ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    }
+                    AnimatedVisibility(
+                        visible = collapsingState.toolbarState.progress < 0.89f
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = anime.name,
+                            style = defaultTextStyle,
+                            fontSize = 18.sp,
+                            color = Color.White,
+                            maxLines = 2,
+                            textAlign = TextAlign.Center,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+                AnimatedVisibility(
+                    visible = collapsingState.toolbarState.progress < 0.89f,
+                    enter = fadeIn() + expandHorizontally(),
+                    exit = fadeOut() + shrinkHorizontally(),
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 8.dp)
+                ) {
+                    IconButton(
+                        onClick = { /* TODO */ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Share,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
         }
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 24.dp)
+                .padding(top = 12.dp)
+                .verticalScroll(scrollState)
         ) {
-            CustomIcon(
-                icon = R.drawable.ic_arrow_forward,
-                text = stringResource(R.string.watch_trailer),
-                size = 24.dp,
-                shape = CircleShape,
-                color = Color.Black.copy(0.5f),
-                textStyle = defaultTextStyle.copy(
-                    fontSize = 14.sp, color = Color.White
-                ),
-                modifier = Modifier.padding(top = 36.dp)
-            )
-            Spacer(modifier = Modifier.height(20.dp))
-            Image(
-                painter = painterResource(anime.poster),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(202.dp)
-                    .clip(RoundedCornerShape(12.dp))
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = anime.name,
-                style = defaultTextStyle,
-                fontSize = 18.sp,
-                color = DarkSlateBlue,
-                maxLines = 2,
-                textAlign = TextAlign.Center,
-                overflow = TextOverflow.Ellipsis
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = anime.episodeDuration,
-                style = defaultTextStyle,
-                fontSize = 14.sp,
-                color = PhilippineGray
-            )
-            Spacer(modifier = Modifier.height(12.dp))
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
