cbe87a0f429230067ebf7308c1beaf84e8403fea
Jeferson Coutinho <jefissuu@gmail.com>
2022-12-05 19:23:11 -0300

    updated constants

--- app/src/main/java/com/jefisu/anlist/data/AnimeConstants.kt
+++ app/src/main/java/com/jefisu/anlist/data/AnimeConstants.kt
@@ -1,7 +1,5 @@
 package com.jefisu.anlist.data
 
 object AnimeConstants {
-    const val GET_ANIME_BACKGROUND = "https://kitsu.io/api/edge/anime?fields[anime]=canonicalTitle,coverImage"
-    const val GET_MANGA_BACKGROUND = "https://kitsu.io/api/edge/manga?fields[manga]=canonicalTitle,coverImage"
     const val BASE_URL = "https://api.jikan.moe/v4"
 }
